9d59c07531c73d79b70452928d355758f707b91a
davidfunes <davidfunes@davidfunes.com>
2023-01-17 11:44:26 +0100

    Ejercicio 2 añadido

---
+++ JavaScript/ejercicio-02/index.js
@@ -0,0 +1,14 @@
+lista = [
+    "David",
+    43,
+    true,
+    new Date("1980-02-27"),
+    {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        date: new Date("1954-07-29"),
+        url: "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings",
+    }
+];
+
+console.log(lista);
